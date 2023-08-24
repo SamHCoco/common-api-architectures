2176eb6d94da40570c8869762cb84fe571fb234c
passvault-app <142117795+passvault-app@users.noreply.github.com>
2023-08-24 23:00:39 +0100

    Initial commit

---
+++ src/test/java/com/samhcoco/systemdesign/commonapisarchitectures/CommonApisArchitecturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhcoco.systemdesign.commonapisarchitectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApisArchitecturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

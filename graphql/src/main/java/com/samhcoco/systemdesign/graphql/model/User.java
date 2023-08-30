d1b49e8bd026c473fec1c0c2a99409bc32273502
passvault-app <142117795+passvault-app@users.noreply.github.com>
2023-08-30 19:00:45 +0100

    GraphQL API WIP

---
+++ graphql/src/main/java/com/samhcoco/systemdesign/graphql/model/User.java
@@ -0,0 +1,21 @@
+package com.samhcoco.systemdesign.graphql.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+}
